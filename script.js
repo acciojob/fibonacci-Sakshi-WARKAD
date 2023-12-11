274ffe85a94f0277015ccf8fd9d24d57429b60f4
Sakshi-WARKAD <89745618+Sakshi-WARKAD@users.noreply.github.com>
2023-12-11 11:47:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function fibonacci(num) {
-// your code here
+function fibonacci(num) {  
+	let sum =0;
+	for(let i=0; i<=num; i++){
+		sum = sum + num;
+	}
+	return sum;
 }
 
 module.exports = fibonacci;
